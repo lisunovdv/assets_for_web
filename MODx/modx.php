64ab9b15293e71bce1e2a5f9ad4f125af9a3de5e
lisunovdv <lisunovdv@gmail.com>
2016-07-09 15:31:20 +0300

    Update modx.php

--- MODx/modx.php
+++ MODx/modx.php
@@ -1,5 +1,6 @@
 [[*description:notempty=`<p>[[*description]]</p>`]]
 [[*tv_tabs:isnot=``:then=``]]
 
+UPDATE `modx_site_htmlsnippets` SET `snippet` = replace(`snippet`, '/storage/', '/assets/images/');
 
 @DATABASE@_%H-%i_%d-%m-%Y

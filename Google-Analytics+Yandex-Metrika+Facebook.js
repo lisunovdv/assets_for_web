580b77d88c80cfd83e33b315654d0cdaa064c9e5
lisunovdv <lisunovdv@gmail.com>
2016-12-27 11:47:25 +0200

    Create Google-Analytics+Yandex-Metrika+Facebook.js

---
+++ Google-Analytics+Yandex-Metrika+Facebook.js
@@ -0,0 +1,16 @@
+function temposSendGoal(goal) {
+    var logData = {};
+    if (typeof ga == 'function') {
+        ga('send', 'event', goal, 'send');
+        logData['google_analytics'] = goal;
+    }
+    if (typeof window.yaCounter00000000.reachGoal == 'function') {
+        window.yaCounter00000000.reachGoal(goal);
+        logData['yandex_metrika'] = goal;
+    }
+    if (typeof fbq == 'function') {
+        fbq('track', 'Lead');
+        logData['facebook_pixel'] = goal;
+    }
+    console.log(logData);
+}

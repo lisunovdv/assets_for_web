0fa805968db22839be8f77b17dab233b435236ef
lisunovdv <lisunovdv@gmail.com>
2017-01-20 10:45:05 +0200

    Update Git commit and push.cmd

--- git/Git commit and push.cmd
+++ git/Git commit and push.cmd
@@ -1,1 +1,7 @@
 git add --all . && git commit -am "upd ver" && (git push || (git pull && git push))
+
+
+test "upd ver"
+
+Содержимое файла test.cmd:
+git add --all . && git commit -am "$1" && (git push || (git pull && git push))
